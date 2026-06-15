aec4beacb7f95407f8c23048c92ff0198dfcadd9
juliisgaes <julianisgaes@gmail.com>
2026-06-15 14:42:02 -0700

    matrices, matrices y más matrices

--- fastback.c
+++ fastback.c
@@ -1,11 +1,14 @@
 /* fastback
  * escrito por juligar en vim btw */
-#include <raylib.h>
-#include <fastback.h>
 
-void main(void) {
-	FastbackTool tool = CreateFastbackTool(0, 0, 0);
+#include <stdlib.h>
+#include <raylib.h>
+#include "include/fastback.h"
+
+int main(void) {
+	FastbackTool tool = CreateFastbackTool(0, 0, 0, 0);
 	InitWindow(500, 500, "fastback");
+	SetTargetFPS(60);
 	while (!WindowShouldClose() && tool.scene < 3) {
 		BeginDrawing();
 		ClearBackground(WHITE);
@@ -13,17 +16,42 @@ void main(void) {
 			case 0:
 				tool.texturesNu = 2;
 				Texture2D* textures = MemAlloc(sizeof(Texture2D) * tool.texturesNu);
-				LoadTextures(&tool, (const char*[]){"resources/sprites/title/title-1.png", "resources/sprites/title/title-2.png"}, textures);
+				if (textures == NULL) {
+					return 1;
+				}
+				LoadTextures(&tool, (const char*[]){"resources/sprites/title/title-1.png",
+				"resources/sprites/title/title-2.png"}, textures);
+				Button* buttons = MemAlloc(sizeof(Button) * tool.buttonsNu);
+				if (buttons == NULL) {
+					return 1;
+				}
+				tool.buttonsNu = 1;
+				LoadButtons(&tool, (Rectangle[]){{0.0, 0.0, 360, 100}}, (const char*[]){"resources/sprites/title/title-1.png",
+				"resources/sprites/title/title-2.png"}, buttons);
+				tool.scene = 1;
 			break;
 			case 1:
-				RenderScene1(&tool, textures);
+				RenderScene1(&tool, textures, buttons);
 			break;
 			case 2:
 				UnloadTextures(&tool, textures);
 				MemFree(textures);
+				textures = NULL;
+				UnloadButtons(&tool, buttons);
+				MemFree(buttons);
+				buttons = NULL;
+				tool.scene = 3;
 			break;
 		}
 		EndDrawing();
+		if (tool.timer >= 1) {
+			tool.timer = 0;
+		}
+		else {
+			tool.timer += GetFrameTime();
+		}
 	}
+
 	CloseWindow();
+	return 0;
 }
--- include/fastback.h
+++ include/fastback.h
@@ -7,16 +7,23 @@
 typedef struct {
 	int scene;
 	int texturesNu;
+	int buttonsNu;
 	double timer;
 } FastbackTool;
+// struct para encapsular botones
+typedef struct {
+	Rectangle dimensions;
+	Texture2D textures[2];
+} Button;
 
 // definiendo funciones para no complicarme la vida
 
 // función para crear una colección de herramientas útiles
-FastbackTool CreateFastbackTool(int scene, int texturesNu, double timer) {
+FastbackTool CreateFastbackTool(int scene, int texturesNu, int buttonsNu, double timer) {
 	FastbackTool tool;
 	tool.scene = scene;
 	tool.texturesNu = texturesNu;
+	tool.buttonsNu = buttonsNu;
 	tool.timer = timer;
 	return tool;
 }
@@ -25,20 +32,46 @@ void LoadTextures(FastbackTool* tool, const char* texturespath[], Texture2D* tex
 	for (int i = 0; i < tool->texturesNu; i++) {
 		textures[i] = LoadTexture(texturespath[i]);
 	}
-	tool->scene = 1;
 }
 // función para descargar texturas
 void UnloadTextures(FastbackTool* tool, Texture2D* textures) {
 	for (int i = 0; i < tool->texturesNu; i++) {
 		UnloadTexture(textures[i]);
 	}
-	tool->scene = 3;
+}
+// función para cargar botones y regresar un array de los mismos
+void LoadButtons(FastbackTool* tool, Rectangle dimensions[], const char* texturespath[], Button* buttons) {
+	// cargarndo dimensiones del botón
+	for (int i = 0; i < tool->buttonsNu; i++) {
+		buttons[i].dimensions = dimensions[i];
+		for (int j = 0; j < 2; j++) {
+			buttons[i].textures[j] = LoadTexture(texturespath[(i * 2) + j]);
+		}
+	}
+}
+// función para descargar botones
+void UnloadButtons(FastbackTool* tool, Button* buttons) {
+	for (int i = 0; i < tool->buttonsNu; i++) {
+		for (int j = 0; j < 2; j++) {
+			UnloadTexture(buttons[i].textures[j]);
+		}
+	}
 }
 
+
 // definiendo funciones para dibujar las escenas
-void RenderScene1(FastbackTool* tool, Texture2D* textures) {
-	DrawTexture(textures[0], 0, 0, WHITE);
+void RenderScene1(FastbackTool* tool, Texture2D* textures, Button* buttons) {
+	// dibujando sprites animados
+	if (tool->timer >= 0.5) {
+		DrawTexture(buttons[0].textures[1], buttons[0].dimensions.x, buttons[0].dimensions.y, WHITE);
+//		DrawTexture(textures[1], 70, 20, WHITE);
+	}
+	else {
+		DrawTexture(buttons[0].textures[0], buttons[0].dimensions.x, buttons[0].dimensions.y, WHITE);
+//		DrawTexture(textures[0], 70, 20, WHITE);
+	}
+	// saliendo de la escena
 	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-		tool->scene = 3;
+		tool->scene = 2;
 	}
 }
